3a151e9e79164941c89c0b5bca15aefb0f4acd68
emege-dev <enrique.melida@gmail.com>
2022-03-24 21:45:57 -0300

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() 
+{ 
+  function formatName( userName )
+  {
+    return userName.toUpperCase( );
+  }
 
+  function getGreetings( userName )
+  {
+    if( userName )
+    {
+      return(  <h1>Hello { formatName( userName ) }!</h1> );
+    }
+    else
+    {
+      return( <h1> Hello Stranger</h1>)
+    }
+  }
+  const name = "John Smith";
+  const userImage= "/logo192.png";
+  const image = <img src= { userImage } alt= "Logo"/>;
+  const element = ( 
+                  <div> 
+                      <h1>Hello {name} </h1>
+                      <p>Good to see you!</p>
+                      { image }
+                  </div>
+  );
+
+
+  return (
+      element 
+    )
+}
 export default App;
